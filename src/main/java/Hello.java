8a5503621798b094c1d017c6d17d1b125781418a
muath22 <40863580+muath22@users.noreply.github.com>
2020-01-19 23:59:23 +0300

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,7 +11,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello WorldNEW: src/main/java/Hello.java";
+      message = "Hello WorldNEW2: src/main/java/Hello.java";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
