681ae06ddc0f5bb1870570760f223429bcfd8fd6
muath22 <40863580+muath22@users.noreply.github.com>
2020-01-20 11:22:25 +0300

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,7 +11,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World";
+      message = "Hello World-V2";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
